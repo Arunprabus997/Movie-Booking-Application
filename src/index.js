10fdacd51a0c54803b585c4e61dada89efa1dc69
Arunprabu <harikrishs932@gmail.com>
2021-08-13 19:44:21 +0530

    Creating the Header Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from "./common/header/Header.js"
 
 
 
 ReactDOM.render(
  <div>
-
+   <Header/>
  </div>,
   document.getElementById('root')
-  
 );
